30cf1b9ad2ca9f954efd830f8da8e3ad78005c08
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-11-25 00:43:16 +0400

    Prepare code

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 // What is canvas?
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
 // https://www.w3schools.com/html/html5_canvas.asp
+// https://www.w3schools.com/graphics/canvas_intro.asp
+// https://www.w3schools.com/graphics/canvas_reference.asp
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 
 // How to create it and use it
 
-// Draw lines
-
 // Draw rectangles
+// =================
+// fillRect
 
-// Draw circle
+// strokeRect
+
+// clearRect
+
+// fillText
+
+// strokeText
+
+// Paths   beginPath, moveTo, lineTo, closePath, stroke, fill
+// ==============
+
+// Arcs
+// ====================
 
 // Draw image
+// ====================
 
 // Write text
+// ====================
+
+// Ball animation
