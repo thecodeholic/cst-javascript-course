508cee29242fe9ffdccca0831ba856cd2c5a3742
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-12-16 08:43:16 +0400

    Prepare practical work

--- homework.js
+++ homework.js
@@ -1,8 +1,38 @@
+// Use bootstrap to make projects nice, if you don't want to loose time on writing CSS
+
 // ========================================================
-// Create notes application which will save notes in localStorage
-// https://youtu.be/Bav5SL8-sI4
+// Task 1 - Lorem ipsum Generator
+/*
+Need possibility to choose what you want to generate: Sentences, paragraphs, words.
+And must choose how many you want to generate.
+*/
+// https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Lorem-Ipsum-Generator.md
+// https://www.npmjs.com/package/lorem-ipsum
 // ========================================================
 
 // ========================================================
-// Create reminder app
+// Task 2 - Meal Searching App
+/*
+ We need one input field, where we type search term and as soon as we hit enter it fetches meals and displays them bellow search
+*/
+// https://www.themealdb.com/api.php
+// ========================================================
+
+// ========================================================
+// Task 3, 4 - Currency Converter
+// https://github.com/florinpop17/app-ideas/blob/master/Projects/2-Intermediate/Currency-Converter.md
+// ========================================================
+
+// ========================================================
+// Task 5 - Submit data to Json Placeholder
+/*
+Create form with the following fields: name, username, email, phone, website.
+When you fill the form and hit submit button it must send POST request on the following URL: https://jsonplaceholder.typicode.com/users.
+All fields are required. If you leave one field empty and submit the form, request must not be make and that field must become red: 
+https://getbootstrap.com/docs/4.2/components/forms/#server-side
+Add button "Load random user" above the table, which generates random number (from 1 to 10) and makes request on the following url: https://jsonplaceholder.typicode.com/users/{random}
+fetches the user and loads that inside the form. 
+When you submit the form it will make put request on the same url from which user was grabbed and update that user.
+Hint: If user does not have ID, you should make POST request, otherwise put request.
+*/
 // ========================================================
