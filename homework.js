bddbc5503abcaf86fde2ad2dfbc86bf23ca0ec41
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-12-16 09:22:39 +0400

    Minor updates in homework

--- homework.js
+++ homework.js
@@ -1,10 +1,16 @@
 // Use bootstrap to make projects nice, if you don't want to loose time on writing CSS
+// Create 4 folders for each task
+// Task1
+// Task2
+// Task3-4
+// Task5
 
 // ========================================================
 // Task 1 - Lorem ipsum Generator
 /*
 Need possibility to choose what you want to generate: Sentences, paragraphs, words.
 And must choose how many you want to generate.
+If you use npm package, don't send node_modules folder. Send me package.json
 */
 // https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Lorem-Ipsum-Generator.md
 // https://www.npmjs.com/package/lorem-ipsum
@@ -13,7 +19,8 @@ And must choose how many you want to generate.
 // ========================================================
 // Task 2 - Meal Searching App
 /*
- We need one input field, where we type search term and as soon as we hit enter it fetches meals and displays them bellow search
+ We need one input field, where we type search term and as soon as we hit enter it fetches meals 
+ and displays them bellow search with image
 */
 // https://www.themealdb.com/api.php
 // ========================================================
@@ -30,9 +37,9 @@ Create form with the following fields: name, username, email, phone, website.
 When you fill the form and hit submit button it must send POST request on the following URL: https://jsonplaceholder.typicode.com/users.
 All fields are required. If you leave one field empty and submit the form, request must not be make and that field must become red: 
 https://getbootstrap.com/docs/4.2/components/forms/#server-side
-Add button "Load random user" above the table, which generates random number (from 1 to 10) and makes request on the following url: https://jsonplaceholder.typicode.com/users/{random}
+Add button "Load random user" above the form, which generates random number (from 1 to 10) and makes request on the following url: https://jsonplaceholder.typicode.com/users/{random}
 fetches the user and loads that inside the form. 
 When you submit the form it will make put request on the same url from which user was grabbed and update that user.
-Hint: If user does not have ID, you should make POST request, otherwise put request.
+Hint: If user does not have ID, you should make POST request, otherwise PUT request.
 */
 // ========================================================
